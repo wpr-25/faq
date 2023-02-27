3fab0dcb7d4b11cec3f8e623ce007c5c76352385
wpr-25 <willy.ok1025@gmail.com>
2023-02-27 18:18:02 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "className",
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
