1aed904c1396e1c89b8ec3c81aa4d1bd1b029043
wpr-25 <willy.ok1025@gmail.com>
2023-03-01 14:01:31 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -119,28 +119,28 @@ export default function NavbarBasicPreview() {
         {/*<!--slider-->*/}
         <div className="carousel max-w-7xl mx-auto py-4 dark:bg-white">
           <div id="slide1" className="carousel-item relative w-full">
-            <img src="3.jpg" className="w-full" />
+            <img src="3.jpg" className="w-full" alt="movi1"/>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide4" className="btn btn-circle">❮</a>
               <a href="#slide2" className="btn btn-circle">❯</a>
             </div>
           </div>
           <div id="slide2" className="carousel-item relative w-full">
-            <img src="4.jpg" className="w-full" />
+            <img src="4.jpg" className="w-full" alt="movi2"/>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide1" className="btn btn-circle">❮</a>
               <a href="#slide3" className="btn btn-circle">❯</a>
             </div>
           </div>
           <div id="slide3" className="carousel-item relative w-full">
-            <img src="5.jpg" className="w-full" />
+            <img src="5.jpg" className="w-full" alt="movi3"/>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide2" className="btn btn-circle">❮</a>
               <a href="#slide4" className="btn btn-circle">❯</a>
             </div>
           </div>
           <div id="slide4" className="carousel-item relative w-full">
-            <img src="2.jpg" className="w-full" />
+            <img src="2.jpg" className="w-full" alt="movi4"/>
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
               <a href="#slide3" className="btn btn-circle">❮</a>
               <a href="#slide1" className="btn btn-circle">❯</a>
